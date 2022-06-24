963363d023044fc46c6a9f11bdbc9ce6655ce3a6
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-06-24 19:26:35 +0300

    Hafta sonu ödevlerinin yer aldığı dosya

---
+++ uygulamalar_1.py
@@ -0,0 +1,47 @@
+# UYGULAMALAR
+# Her uygulama örneği bir yorum parçasının altına yapılabilir
+# //////////////////////////////////////////////////////////////////////////////
+
+# 1- Kullanıcıdan "Öğrenci Adı", "1. Yazılı Notu" ve "2. Yazılı Notu"
+# bilgilerini alın. 2 yazılı notunun ortalamasını hesaplayıp, ekrana
+# öğrencinin adı ve not ortalamasının yer aldığı bir cümle şeklinde
+# çıktı verin.
+
+
+# 2- Kullanıcıdan yarı çapı alınan bir dairenin alanını ve çevresini
+# hesaplayınız
+# Daire Alanı   : πr²
+# Daire Çevresi : 2πr
+#
+# * Yarı çap kullanıcıdan alınacak: r
+# * π: 3.14
+
+
+url = "https://teknolojiaihl.meb.k12.tr/"
+okul  = "Teknoloji Anadolu İmam Hatip Lisesi"
+
+# 3- 'url' içinde kaç karakter bulunmaktadır?
+
+
+# 4- 'Teknoloji Anadolu İmam Hatip Lisesi' ifadesinin tamamını büyük harf
+# yazdırın. Türkçe'deki küçük i harflerinin büyük harfe geçince I olması
+# sorununun önüne geçin.
+
+
+# 5- okul değişkeni içinde yer alan karakter dizisi içinde belirli bir ifadeyi
+# arayıp indeks numarasını alalım: find()
+
+# 6- okul değişkeni içinde yer alan karakter dizisi içindeki karakterlerin
+# hepsi alfabetik mi: isalpha()
+
+# 7- okul değişkeni içinde yer alan karakter dizisi içindeki karakterlerin
+# hepsi sayısal mı: isdigit()
+
+# 8- okul değişkeni içinde yer alan karakter dizisi belirli bir ifade ile mi
+# başlıyor: startswith()
+
+# 9- okul değişkeni içinde yer alan karakter dizisi belirli bir ifade ile mi
+# bitiyor: endswith()
+
+# 10- okul değişkeni içinde yer alan karakter dizisinde belirli bir ifadeyi
+# başka bir ifade ile yer değiştirelim: replace()
