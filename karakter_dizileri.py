08aebb9fdb46387689e8907a44e40fa99c930857
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-06-24 16:11:49 +0300

    karakter dizileri çalışıldı

---
+++ karakter_dizileri.py
@@ -0,0 +1,12 @@
+ad = "Eren"
+egitim = "Bot Yazma Eğitimi"
+
+# str veri tipindeki değerin uzunluğuna bakalım (len())
+# print(len(ad))
+# print(len(egitim))
+
+# str değer içindeki elemanlara tek tek ulaşalım
+print(ad[0])
+print(ad[1])
+print(ad[2])
+print(ad[3])
