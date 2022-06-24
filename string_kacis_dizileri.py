b7358d879aef39138790580eecbf997b1b03242f
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-06-24 17:38:46 +0300

    String kaçış ifadeleri çalışıldı

---
+++ string_kacis_dizileri.py
@@ -0,0 +1,25 @@
+# print("Eren, "Bugün sinemaya gidiyorum" dedi")
+# print('Eren, "Bugün sinemaya gidiyorum" dedi')
+# print("Eren, \"Bugün sinemaya gidiyorum\" dedi")
+
+# print('Yarın Adana'ya gideceğim')
+# print('Yarın Adana\'ya gideceğim')
+
+# Uzun ifadeleri bölmek için kullanabiliriz.
+# print("Geçerli sütuna ve verilen sekme boyutuna bağlı \
+#        olarak, tüm sekme karakterlerinin bir veya daha \
+#       fazla boşlukla değiştirildiği dizenin bir kopyasını \
+#       döndürün. Sekme konumları, her sekme boyutu \
+#        karakterinde oluşur (varsayılan 8'dir \
+#        ,  0, 8, 16 ve benzeri sütunlarda sekme konumları verir).")
+
+# Metni alt satıra indirmek için \n karakteri kullanılır
+# print("Bot\nYazma\nEğitimi")
+
+# "C:\Users\erenMustafaOzdal\Desktop\TAİHL - Bot Yazma Eğitimi\code" ekrana yazalım
+# print("C:\\Users\\erenMustafaOzdal\\Desktop\\TAİHL - Bot Yazma Eğitimi\\code")
+# print("C:/Users/erenMustafaOzdal/Desktop/TAİHL - Bot Yazma Eğitimi/code")
+
+# TAB tuşu anlamına gelen sekme işareti \t
+print("Bot\tYazma\tEğitimi")
+
