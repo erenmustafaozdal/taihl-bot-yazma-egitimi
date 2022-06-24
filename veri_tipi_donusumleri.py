8c52e5b0a903b1559b7dc1949ff3f290905ef4b4
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-06-24 15:29:31 +0300

    veri tipi dönüşümleri konusuna başlangıç yapıldı

---
+++ veri_tipi_donusumleri.py
@@ -0,0 +1,8 @@
+# Kullanıcıdan iki sayı alıp bu sayıları toplayan uygulama
+sayi1 = input("1. sayıyı yazın: ")
+sayi2 = input("2. sayıyı yazın: ")
+
+# print(sayi1 + sayi2)  # sonuç: 74
+# print(type(sayi1))
+# print(type(sayi2))
+# print(int(sayi1) + int(sayi2))
