422c9542f310ccf5fd4782b008884297cced961a
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-06-24 16:47:37 +0300

    Create string_metotlari.py

---
+++ string_metotlari.py
@@ -0,0 +1,20 @@
+ders = "Bot Yazma Eğitimi"
+
+# Tüm karakterleri büyük yazalım
+print(ders.upper())
+
+# Tüm harfleri küçük yapalım
+lower = ders.lower()
+print(lower)
+
+# Her kelimenin ilk harfini büyük yapalım
+print(lower.title())
+
+# Sadece ilk karakteri büyük yazalım
+print(lower.capitalize())
+
+# Belirli bir ifadenin kaç defa geçtiğini bulalım
+print(ders.count("i"))
+print(ders.count(" "))
+print(ders.count("Bot"))
+
