ea8676edd7890190b2bb4a1f99ab15fa3264d0eb
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-06-24 15:46:46 +0300

    veri dönüşümleri güncellendi

--- veri_tipi_donusumleri.py
+++ veri_tipi_donusumleri.py
@@ -1,8 +1,35 @@
 # Kullanıcıdan iki sayı alıp bu sayıları toplayan uygulama
-sayi1 = input("1. sayıyı yazın: ")
-sayi2 = input("2. sayıyı yazın: ")
+# sayi1 = input("1. sayıyı yazın: ")
+# sayi2 = input("2. sayıyı yazın: ")
 
 # print(sayi1 + sayi2)  # sonuç: 74
 # print(type(sayi1))
 # print(type(sayi2))
 # print(int(sayi1) + int(sayi2))
+
+# Farklı veri tiplerini değişkenlere atayalım ve tiplerine bakalım (type())
+x = 1  # integer
+z = "1"  # string
+y = 1.2  # float
+ad = "Eren"  # string
+sinav_basarili_mi = True  # bool
+# print("x: ", type(x))
+# print("z: ", type(z))
+# print("y: ", type(y))
+# print("ad: ", type(ad))
+# print("sinav_basarili_mi: ", type(sinav_basarili_mi))
+
+# Tip dönüştürme yapalım
+# int to float
+# print(float(x))
+
+# str to int
+# print(int(z))
+# print( type( int(z) ) )
+
+# bool to str
+# print(str(sinav_basarili_mi))
+# print(type(str(sinav_basarili_mi)))
+
+# bool to int
+print(int(sinav_basarili_mi))
