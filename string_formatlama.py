d667d2e47ad2ce0b30e91782075ddd17e9ab57ef
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-06-24 16:29:17 +0300

    Update string_formatlama.py

--- string_formatlama.py
+++ string_formatlama.py
@@ -4,5 +4,13 @@ soyad = "Özdal"
 yas = "36"
 
 # Değişkenlerdeki bilgileri birleştirelim
-print("Adınız: " + ad + ", Soyadınız: " + soyad + ", Yaşınız: " + yas)
-print("Adınız: " + ad + "\nSoyadınız: " + soyad + "\nYaşınız: " + yas)
+# print("Adınız: " + ad + ", Soyadınız: " + soyad + ", Yaşınız: " + yas)
+# print("Adınız: " + ad + "\nSoyadınız: " + soyad + "\nYaşınız: " + yas)
+
+# print("Adınız: {}, Soyadınız: {}, Yaşınız: {}".format(ad, soyad, yas))
+# print("Adınız: {2}, Soyadınız: {0}, Yaşınız: {1}".format(ad, soyad, yas))
+# print("Adınız: {x}, Soyadınız: {y}, Yaşınız: {z}".format(x=ad, y=soyad, z=yas))
+
+# f string ile formatlama
+print(f"Adınız: {ad}, Soyadınız: {soyad}, Yaşınız: {yas}")
+
