205bbf54c9188a31a88040e3cebdefd4a181d20a
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-06-24 16:19:01 +0300

    Create string_formatlama.py

---
+++ string_formatlama.py
@@ -0,0 +1,8 @@
+# Değişkenler oluşturalım
+ad = "Eren Mustafa"
+soyad = "Özdal"
+yas = "36"
+
+# Değişkenlerdeki bilgileri birleştirelim
+print("Adınız: " + ad + ", Soyadınız: " + soyad + ", Yaşınız: " + yas)
+print("Adınız: " + ad + "\nSoyadınız: " + soyad + "\nYaşınız: " + yas)
