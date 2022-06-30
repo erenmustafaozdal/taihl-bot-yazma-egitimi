d40e63eda7c1453601c144b92065a0a3bd9ebcf8
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-06-30 16:26:56 +0300

    Name ile elemana ulaştık

--- elemanlara_ulasma.py
+++ elemanlara_ulasma.py
@@ -13,26 +13,29 @@ from selenium.webdriver.common.by import By
 from time import sleep
 
 tarayici = webdriver.Chrome(ChromeDriverManager().install())
+tarayici.maximize_window()
 tarayici.get("https://www.amazon.com.tr")
 
 # class name ile elemana ulaşma
 # 1. arama kutusunu verir
 # arama_kutusu = tarayici.find_element(By.CLASS_NAME, "nav-input")
 # Bütün arama kutularını döndürür
-arama_kutulari = tarayici.find_elements(By.CLASS_NAME, "nav-input")
+# arama_kutulari = tarayici.find_elements(By.CLASS_NAME, "nav-input")
 # # print("arama_kutusu: ", arama_kutusu)
 # print("arama_kutulari sayisi: ", len(arama_kutulari))
 # print("arama_kutulari: ", arama_kutulari)
 
-# id ile elemana ulaşma
+# id ile elemana ulaşma (Çerez penceresini kapatıyorz)
 tarayici.find_element(By.ID, "sp-cc-accept").click()
 
 # 1.si arama kutusu
-arama_kutulari[0].send_keys("Bluetooth kulaklık")
+# arama_kutulari[0].send_keys("Bluetooth kulaklık")
 # 2.si arama tuşu
-arama_kutulari[1].click()
+# arama_kutulari[1].click()
 
 # name özniteliği ile elemana ulaşma
+arama_kutusu = tarayici.find_element(By.NAME, "field-keywords")
+arama_kutusu.send_keys("Kulaklık")
 
 # bağlantı metni ile elemana ulaşma
 
