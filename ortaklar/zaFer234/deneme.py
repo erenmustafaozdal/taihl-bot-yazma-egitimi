5e1434388b26ad5c4af88b11751e7aa211f0006c
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-07-05 09:38:39 +0300

    Update deneme.py

--- ortaklar/zaFer234/deneme.py
+++ ortaklar/zaFer234/deneme.py
@@ -2,12 +2,16 @@ from openpyxl import Workbook, load_workbook
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import os
 
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install())
+)
 driver.get("https://www.igrus.com/guzel-sozler-kisa-anlamli-ve-bir-o-kadar-harika/?gclid=Cj0KCQjwn4qWBhCvARIsAFNAMiipOWH_G-D9pNpoKTCijFfr-Tk-qk6oBZvpVuhR4rgRMyFOH-NxyWAaArT2EALw_wcB")
 
 
