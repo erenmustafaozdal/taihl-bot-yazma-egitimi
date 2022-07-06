fb78d4cea5100b532794e8f0b96220eaf69f4e13
Ahmet Serdar SONGUR <songuras51@gmail.com>
2022-07-06 10:40:14 +0300

    bim denemesi 2

--- modules/bim.py
+++ modules/bim.py
@@ -9,7 +9,9 @@ def resimleri_al(tarayici):
     today = date.today()
     tarayici.maximize_window()
     tarayici.get("https://www.bim.com.tr/categories/100/aktuel-urunler.aspx")
+    nextweek = tarayici.find_element(By.XPATH, "//div[@class='tabArea']//a[1]").click
+    tarayici.find_element(By.CLASS_NAME,"bigArea col-8 col-md-9").click()
     sleep(5)
-    resim = f"resimler/{today}.png"
-    tarayici.find_element(By.CLASS_NAME, "row").screenshot(resim)
+    resim = f"resimler/bim{today}.png"
+    tarayici.find_element(By.CLASS_NAME, "fancybox-image").screenshot(resim)
     return resim_yollari
