d0ed89a24bcf82a076e0bb732be3fcc95094b18d
SpaceAmbasador <107392483+SpaceAmbasador@users.noreply.github.com>
2022-07-06 10:05:48 +0300

    Gratis'_Katalog_Pdf_download

--- modules/gratis.py
+++ modules/gratis.py
@@ -1,5 +1,17 @@
+from time import sleep
+
+
 def resimleri_al(tarayici):
+
     resim_yollari = []
+
+    # from selenium.webdriver.common.by import By
+
+    tarayici.maximize_window()
+    tarayici.get(
+        "https://view.publitas.com/72755/1500235/pdfs/b8fd8f3e-0bdb-4cb9-933e-ae1d8b62c485.pdf?response-content"
+        "-disposition=attachment%3B+filename%2A%3DUTF-8%27%27GRATIS%2520-%2520TEMMUZ%2520publish.pdf")
+    sleep(3)
 
 
     return resim_yollari
