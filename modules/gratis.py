8c43884d2ce07532a5587421292fedec6989a02b
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-07-06 11:54:42 +0300

    Update gratis.py

--- modules/gratis.py
+++ modules/gratis.py
@@ -1,21 +1,30 @@
 from time import sleep
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from modules.yardimcilar import download_wait, pdf2image
+from datetime import date
 
 
 def resimleri_al(tarayici: WebDriver):
     resim_yollari = []
 
     tarayici.maximize_window()
-    tarayici.get("https://www.gratis.com/katalog?k=gratis%20katalog&gclid=CjwKCAjwwo"
-                 "-WBhAMEiwAV4dybWIU9qHGXWe2Nus5SjtDMJrSQR4lfPDJzFBe7KyoShrDMFPhGBLerRoCeWwQAvD_BwE&gclsrc=aw.ds")
+    tarayici.get("https://view.publitas.com/gratis")
 
-    sleep(7)
+    bekle = WebDriverWait(tarayici, 30)
+    bekle.until(ec.visibility_of_element_located(
+        (By.XPATH, '//a[@data-href="download_pdf"]')
+    )).click()
 
-    tarayici.find_element(By.XPATH, '//a[@data-href="download_pdf"]').click()
+    pdf = download_wait()
+    resimler = pdf2image(pdf)
+    today = date.today()
 
-    sleep(2)
-
-
+    for i in range(len(resimler)):
+        resim = f"resimler/gratis/{today}-{i}.png"
+        resimler[i].save(resim, 'PNG')
+        resim_yollari.append(resim)
 
     return resim_yollari
