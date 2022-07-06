3d41ec444a4b79d779586083c1194978c24d0297
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2022-07-06 11:02:47 +0300

    Update bim.py

--- modules/bim.py
+++ modules/bim.py
@@ -8,10 +8,25 @@ def resimleri_al(tarayici):
     resim_yollari = []
     today = date.today()
     tarayici.maximize_window()
-    tarayici.get("https://www.bim.com.tr/categories/100/aktuel-urunler.aspx")
-    nextweek = tarayici.find_element(By.XPATH, "//div[@class='tabArea']//a[1]").click
-    tarayici.find_element(By.CLASS_NAME,"bigArea col-8 col-md-9").click()
-    sleep(5)
-    resim = f"resimler/bim{today}.png"
+    tarayici.get("https://www.bim.com.tr/Categories/680/afisler.aspx")
+    sleep(2)
+    # çerezleri kabul et
+    tarayici.find_element(By.XPATH, "//button[text()='Çerezleri kabul edin']").click()
+    tarayici.find_element(By.XPATH, "//a[normalize-space()='GELECEK HAFTA']").click()
+    sleep(2)
+    fancyboxs = tarayici.find_elements(By.XPATH,"//a[@class='fancyboxImage']")
+    fancyboxs[-1].click()
+    sleep(2)
+    resim_index = 1
+    resim = f"resimler/bim/{today}-{resim_index}.png"
     tarayici.find_element(By.CLASS_NAME, "fancybox-image").screenshot(resim)
+    resim_yollari.append(resim)
+    while True:
+        resim_index += 1
+        resim = f"resimler/bim/{today}-{resim_index}.png"
+
+
+        resim_yollari.append(resim)
+
+
     return resim_yollari
