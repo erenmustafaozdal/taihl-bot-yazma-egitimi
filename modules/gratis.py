ca986bbfe2ea7c5532a9c54fdba1c718b00710f0
SpaceAmbasador <107392483+SpaceAmbasador@users.noreply.github.com>
2022-07-06 10:44:35 +0300

    Gratis_Trend_Katalog

--- modules/gratis.py
+++ modules/gratis.py
@@ -1,17 +1,21 @@
 from time import sleep
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 
 
-def resimleri_al(tarayici):
-
+def resimleri_al(tarayici: WebDriver):
     resim_yollari = []
 
-    # from selenium.webdriver.common.by import By
-
     tarayici.maximize_window()
-    tarayici.get(
-        "https://view.publitas.com/72755/1500235/pdfs/b8fd8f3e-0bdb-4cb9-933e-ae1d8b62c485.pdf?response-content"
-        "-disposition=attachment%3B+filename%2A%3DUTF-8%27%27GRATIS%2520-%2520TEMMUZ%2520publish.pdf")
-    sleep(3)
+    tarayici.get("https://www.gratis.com/katalog?k=gratis%20katalog&gclid=CjwKCAjwwo"
+                 "-WBhAMEiwAV4dybWIU9qHGXWe2Nus5SjtDMJrSQR4lfPDJzFBe7KyoShrDMFPhGBLerRoCeWwQAvD_BwE&gclsrc=aw.ds")
+
+    sleep(7)
+
+    tarayici.find_element(By.XPATH, '//a[@data-href="download_pdf"]').click()
+
+    sleep(2)
+
 
 
     return resim_yollari
